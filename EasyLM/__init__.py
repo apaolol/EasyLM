0ffa6a3fdf7a7f73a1a6474000a2a41a02c59f2b
apaolol <bautrokyle@gmail.com>
2025-12-07 16:57:06 +0800

    FROMM 200+ LINES TO 80 KAPIN LINES!

--- EasyLM/__init__.py
+++ EasyLM/__init__.py
@@ -8,6 +8,10 @@ from .model_comparator import ModelComparator
 from .plot_helper import PlotHelper
 from .summary_formatter import SummaryFormatter
 from .exceptions import EasyLMError, FitError, PredictError
+from .data_preprocessor import DataPreprocessor
+from .regression_stats import RegressionStats
+
+# Backward compatibility
 from .utils import add_constant, check_array
 
 __all__ = [
@@ -19,6 +23,8 @@ __all__ = [
     "EasyLMError",
     "FitError",
     "PredictError",
+    "DataPreprocessor",
+    "RegressionStats",
     "add_constant",
     "check_array",
 ]
